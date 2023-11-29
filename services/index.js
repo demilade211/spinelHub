6b6552bade7e87ced020756dd05a3f491fc72b19
demilade211 <afolabiogunbanwo@gmail.com>
2023-11-29 06:21:39 +0100

    added next env

--- services/index.js
+++ services/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import cookie from "js-cookie"
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 // Create an instance of Axios with a base URL
 const instance = axios.create({
-  baseURL: 'http://spinel-hub-env-2.eba-ad9m9bcp.us-east-1.elasticbeanstalk.com/', // Replace with your base URL
+  baseURL: `${apiUrl}`, // Replace with your base URL
   headers: {Authorization:cookie.get("token")}
 });
 
-export const baseURL = "http://spinel-hub-env-2.eba-ad9m9bcp.us-east-1.elasticbeanstalk.com/"
+export const baseURL = "Spinel-hub-env-2.eba-ad9m9bcp.us-east-1.elasticbeanstalk.com"
 
 
 
