467c8e0fb894f16395d0ae2f362d37f619ff4258
demilade211 <afolabiogunbanwo@gmail.com>
2023-11-29 03:41:37 +0100

    changed base url to ebs url

--- services/index.js
+++ services/index.js
@@ -3,11 +3,11 @@ import cookie from "js-cookie"
 
 // Create an instance of Axios with a base URL
 const instance = axios.create({
-  baseURL: 'https://spinel-backend.onrender.com', // Replace with your base URL
+  baseURL: 'Spinel-hub-env-2.eba-ad9m9bcp.us-east-1.elasticbeanstalk.com', // Replace with your base URL
   headers: {Authorization:cookie.get("token")}
 });
 
-export const baseURL = "https://spinel-backend.onrender.com"
+export const baseURL = "Spinel-hub-env-2.eba-ad9m9bcp.us-east-1.elasticbeanstalk.com"
 
 
 
